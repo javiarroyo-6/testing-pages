c26159b13039d5347a5542cf2c531399f1178f90
Javier Arroyo <javierarroyo@Javiers-MacBook-Air.local>
2022-03-15 16:10:15 -0700

    Added Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import About from "./pages/About";
-import { Link, Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/about"> About </Link>
-          </li>
-          <li>
-            <Link to="/contact"> Contact </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
+        <Route exact path="/products" element={<Products />} />
         <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </>
