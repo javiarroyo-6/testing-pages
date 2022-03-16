35b1e432fbb94ce14df86655dbd16effd316046e
Javier Arroyo <javierarroyo@Javiers-MacBook-Air.local>
2022-03-15 20:18:58 -0700

    Added contact and products page with form fully connected to gmail

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import "./Contact.css";
 
 const Contact = () => {
+  const [values, setValues] = useState({
+    fullName: "",
+    businessName: "",
+    email: "",
+  });
+
+  const [valid, setValid] = useState(false);
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleFullNameInputChange = (event) => {
+    setValues({ ...values, fullName: event.target.value });
+  };
+
+  const handleBusinessNameInputChange = (event) => {
+    setValues({ ...values, businessName: event.target.value });
+  };
+
+  const handleEmailInputChange = (event) => {
+    setValues({ ...values, email: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (values.fullName && values.businessName && values.email) {
+      setValid(true);
+    }
+
+    emailjs
+      .sendForm(
+        "service_u935tom",
+        "template_w05sjlh",
+        event.target,
+        "1WFe7jRgMJERnt5BF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    setSubmitted(true);
+  };
+
   return (
-    <div>
-      <h1> Contact Page </h1>
+    <div className="form-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+        {submitted && valid ? (
+          <div class="success-message">
+            Success! Thank you for your interest.
+          </div>
+        ) : null}
+        <input
+          onChange={handleFullNameInputChange}
+          value={values.fullName}
+          id="full-name"
+          className="form-field"
+          type="text"
+          placeholder="Full Name"
+          name="fullName"
+        />
+        {submitted && !values.fullName ? (
+          <span id="full-name-error">Please enter a full name.</span>
+        ) : null}
+        <input
+          onChange={handleBusinessNameInputChange}
+          value={values.businessName}
+          id="business-name"
+          className="form-field"
+          type="text"
+          placeholder="Business name"
+          name="businessName"
+        />
+
+        {submitted && !values.businessName ? (
+          <span id="business-name-error">Please enter a business name.</span>
+        ) : null}
+        <input
+          onChange={handleEmailInputChange}
+          value={values.email}
+          id="email"
+          className="form-field"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        {submitted && !values.email ? (
+          <span id="email-error">Please enter an email address.</span>
+        ) : null}
+        <button className="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
 
 export default Contact;
+
+// lcortes@diversified-af.com
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,63 @@
 import React from "react";
+import "../App.css";
+import Footer from "../components/Footer";
 
-const Products = () => {
-  return <div>Products</div>;
-};
+export default function Products() {
+  const products = [
+    "Air Inlets",
+    "Auxiliary Power Units",
+    "Bails",
+    "Body Assemblies",
+    "Brackets",
+    "Cabin Presure Control Systems",
+    "Carrier Assemblies",
+    "Chambers",
+    "Cover Assemblies",
+    "Covers",
+    "Diffusers",
+    "Electrial Systems",
+    "End Bells",
+    "Fans",
+    "Flappers",
+    "Fuel Controls",
+    "Fuel Pump",
+    "Gear Box Housings",
+    "Housings",
+    "Hydraulic Systems",
+    "Impellers",
+    "Liners",
+    "Manifolds",
+    "Next Higher Assembly",
+    "Nozzles",
+    "Pneumatic Systems",
+    "Poppets",
+  ];
 
-export default Products;
+  return (
+    <>
+      <h1 className="products" style={{ height: 200 }}>
+        PRODUCTS
+      </h1>
+      <div style={{ paddingLeft: 100 }}>
+        <h2>
+          Diversified Engineering has demonstrated expertise in manufacturing a
+          wide range of products from a diverse range of materials.
+          <br />
+        </h2>
+        <h2 style={{ paddingTop: 30 }}>
+          Product types include the following:{" "}
+        </h2>
+        <div style={{ paddingLeft: 100 }}>
+          <ul style={{ margin: 20 }}>
+            {products.map((name, index) => (
+              <li id={index} style={{ paddingTop: 10 }}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <Footer id="footer" />
+    </>
+  );
+}
